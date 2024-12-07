6f90e7d6947be6f2a445d86f9a0c20b6eee00a55
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2024-12-07 18:57:40 +0330

    feat (src/js/config.js): create config file to store API key and URL

---
+++ src/js/config.js
@@ -0,0 +1,9 @@
+/**
+ * The API key for accessing the omdbapi API.
+ */
+export const API_KEY = "39558b05";
+/**
+ * The base URL for the omdbapi API.
+ * All API requests will be made to this endpoint.
+ */
+export const API_URL = "https://www.omdbapi.com/?";
