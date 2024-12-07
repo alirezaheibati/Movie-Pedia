5692d7be252af0d35048d228a23029df34b02419
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2024-12-07 18:51:46 +0330

    feat (src/js/controllers/MovieController): create MovieController class

---
+++ src/js/controllers/MovieController.js
@@ -0,0 +1,11 @@
+/**
+ * MovieController class that manages the interaction between the model and views.
+ * It handles user interactions, fetches movie data, and updates the views accordingly.
+ */
+export class MovieController {
+  /**
+   * Constructor for the MovieController class.
+   * Initializes the model, and sets up event handlers for user interactions.
+   */
+  constructor() {}
+}
