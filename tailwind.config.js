9e267235b8fa2b0ac5759460b1d0f4e1745af07d
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2024-12-12 12:24:35 +0330

    fix (tailwind.config.js): correct content path in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*/{html,js}"],
+  content: ["./index.html", "./src/js/**/*.{html,js}"],
   theme: {
     extend: {
       fontFamily: {
