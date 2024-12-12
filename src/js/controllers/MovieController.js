02d8a713aae792712e329dccd861fe3268a14286
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2024-12-12 16:29:34 +0330

    feat (src/js/controllers/MovieController.js): implement renderTopTenMovies method to MovieController class

--- src/js/controllers/MovieController.js
+++ src/js/controllers/MovieController.js
@@ -1,3 +1,5 @@
+import topTenView from "../views/TopTenView";
+import MovieModel from "../models/MovieModel";
 /**
  * MovieController class that manages the interaction between the model and views.
  * It handles user interactions, fetches movie data, and updates the views accordingly.
@@ -7,5 +9,14 @@ export class MovieController {
    * Constructor for the MovieController class.
    * Initializes the model, and sets up event handlers for user interactions.
    */
-  constructor() {}
+  constructor() {
+    this.movieModel = new MovieModel();
+    this.renderTopTenMovies();
+  }
+  /**
+   * Renders the top ten movies using the model.
+   */
+  renderTopTenMovies() {
+    topTenView.render(this.movieModel.topTen);
+  }
 }
