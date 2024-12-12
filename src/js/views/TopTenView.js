0ca7f0e6277ac6cc4d70168e3b2b5a8c562412d5
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2024-12-12 12:22:23 +0330

    feat (src/js/views/TopTenView.js): implement _generateMarkup method for TopTenView class

--- src/js/views/TopTenView.js
+++ src/js/views/TopTenView.js
@@ -10,4 +10,36 @@ class TopTenView extends View {
    * @protected
    */
   _parentElement = document.getElementById("slider-container");
+  /**
+   * Generates the markup for the highlights view.
+   *
+   * @returns {string} The HTML markup for the weather highlights.
+   * @protected
+   */
+  _generateMarkup() {
+    return this._data
+      .map((movie) => {
+        return `
+            <div class="w-[9%] h-full flex justify-start items-end text-slate-50 relative rounded-xl bg-cover bg-no-repeat after:content-[''] after:absolute after:left-0 after:top-0 after:w-full after:h-full after:rounded-xl  after:bg-gradient-to-t after:from-[#041023] after:to-transparent"
+            style="background-image: url(${movie.Poster});">
+                <div class="z-10 w-full p-4">
+                    <button class="absolute right-4 top-4 text-3xl"><i class="fa-regular fa-heart text-[#ea2a49]"></i></button>
+                    <h3 class="text-2xl whitespace-nowrap overflow-hidden text-ellipsis">${movie.Title}</h3>
+                    <p class="mb-1 text-slate-200">${movie.Genre}</p>
+                    <div class="flex justify-start items-start gap-3">
+                        <p class="text-amber-500">
+                            <i class="fa-solid fa-star-half-stroke "></i> ${movie.imdbRating}
+                        </p>
+                        <p class="text-amber-500">
+                            <i class="fa-solid fa-clock-rotate-left text-sm"></i> ${movie.Runtime}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        `;
+      })
+      .join();
+  }
 }
+const topTenView = new TopTenView();
+export default topTenView;
