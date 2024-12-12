9c3c3887566e83c2a5f8598f40b5aea465d053a5
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2024-12-12 16:41:06 +0330

    feat (src/js/views/TopTenSliderViwe.js): create TopTenSliderView class

---
+++ src/js/views/TopTenSliderViwe.js
@@ -0,0 +1,13 @@
+/**
+ * Represents the view for top ten movies slider buttons.
+ */
+class TopTenSliderView {
+  /**
+   * The parent element in the DOM where the top ten movies slider buttons are rendered.
+   * @type {HTMLElement}
+   * @protected
+   */
+  _parentElement = document.getElementById("top-ten-slider");
+}
+const topTenSliderView = new TopTenSliderView();
+export default topTenView;
