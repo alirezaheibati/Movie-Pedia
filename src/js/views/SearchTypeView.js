33b24b93483d9ea054ef8fd822e947fc015693b0
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2024-12-13 16:29:00 +0330

    feat (src/js/views/SearchTypeView.js): implement addHandlerSearchTypeSelector method

--- src/js/views/SearchTypeView.js
+++ src/js/views/SearchTypeView.js
@@ -8,6 +8,28 @@ class SearchTypeView {
    * @protected
    */
   _parentElement = document.getElementById("search-type-container");
+  /**
+   * Adds an event handler to the search type selector buttons.
+   * add and removes proper classes to show the the active selection.
+   * @param {Function} handle - The function to handle the search type selection.
+   */
+  addHandlerSearchTypeSelector(handle) {
+    this._parentElement.addEventListener("click", (e) => {
+      const btn = e.target.closest(".type-toggler-btn");
+      if (!btn) return;
+      const togglerBtns =
+        this._parentElement.querySelectorAll(".type-toggler-btn");
+      togglerBtns.forEach((toggler) =>
+        toggler.classList.remove("text-[#ea2a49]")
+      );
+      btn.classList.add("text-[#ea2a49]");
+      if (btn.classList.contains("movie-toggler")) {
+        handle("t");
+      } else {
+        handle("s");
+      }
+    });
+  }
 }
 const searchTypeView = new SearchTypeView();
 export default searchTypeView;
