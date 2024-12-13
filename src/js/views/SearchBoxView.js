06c53e68230529ce0c08f3806d391a89758b904d
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2024-12-13 11:47:04 +0330

    feat (src/js/views/SearchBoxView.js): create SearchBoxView.js class

---
+++ src/js/views/SearchBoxView.js
@@ -0,0 +1,11 @@
+/**
+ * Represents the view for search box form.
+ */
+class SearchBoxView {
+  /**
+   * The parent element in the DOM where the search box is rendered.
+   * @type {HTMLElement}
+   * @protected
+   */
+  _parentElement = document.getElementById("search-box-form");
+}
