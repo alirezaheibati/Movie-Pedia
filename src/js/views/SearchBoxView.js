33f976bf902d7cd2eba960c6f6b717c562ec8c8e
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2024-12-13 16:03:34 +0330

    feat (src/js/views/SearchBoxView.js): implement addHandlerToSearchFormBtn method

--- src/js/views/SearchBoxView.js
+++ src/js/views/SearchBoxView.js
@@ -8,4 +8,25 @@ class SearchBoxView {
    * @protected
    */
   _parentElement = document.getElementById("search-box-form");
+  /**
+   * Adds an event handler to the search form button for expanding the search input,
+   * and assign proper classes.
+   */
+  addHandlerToSearchFormBtn() {
+    this._parentElement.addEventListener("click", (e) => {
+      const searchActivatorBtn = e.target.closest("#movie-search-btn");
+      if (!searchActivatorBtn) return;
+      const searchInputContainer = this._parentElement.querySelector(
+        "#search-box-container"
+      );
+      searchInputContainer.classList.remove("w-12");
+      searchInputContainer.classList.add("w-full");
+      //change the type of button inside of form from 'button' to 'submit'.
+      setTimeout(() => {
+        searchActivatorBtn.type = "submit";
+      }, 10);
+    });
+  }
 }
+const searchBoxView = new SearchBoxView();
+export default searchBoxView;
