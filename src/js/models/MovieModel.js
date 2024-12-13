400dda5897875a1d2942278b2e7cfdbb2efdcce8
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2024-12-13 22:38:56 +0330

    feat (src/js/models/MovieModel.js): implement loadMoviInformation method

--- src/js/models/MovieModel.js
+++ src/js/models/MovieModel.js
@@ -1,4 +1,6 @@
 import topTenMovies from "../../data/top-ten-data";
+import { getJSON } from "../helper.js";
+import { API_KEY, API_URL } from "../config.js";
 /**
  * Represents the information model for movies.
  */
@@ -14,6 +16,7 @@ export default class MovieModel {
      * 's' represents serial titile search
      */
     this.searchType = "movie";
+    this.searchResults = [];
   }
   /**
    * Sets the search type.
@@ -21,5 +24,20 @@ export default class MovieModel {
    */
   setSearchType(type) {
     this.searchType = type;
+  }
+  /**
+   * Loads movie information based on the search query and current search type.
+   * @param {string} query - The search query.
+   * @throws Will throw an error if the fetch operation fails.
+   */
+  async loadMoviInformation(query) {
+    try {
+      const data = await getJSON(
+        `${API_URL}apikey=${API_KEY}&s=${query}&type=${this.searchType}`
+      );
+      this.searchResults = data;
+    } catch (err) {
+      throw err;
+    }
   }
 }
