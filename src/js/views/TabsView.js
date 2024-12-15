2d95e01a33e744daf9973d4214274a1baf429e2a
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2024-12-15 16:27:04 +0330

    feat (src/js/views/TabsView.js): implement _removeTabsStyle method

--- src/js/views/TabsView.js
+++ src/js/views/TabsView.js
@@ -8,6 +8,15 @@ class TabsView {
    * @protected
    */
   _parentElement = document.getElementById("tabs-container-ul");
+  /**
+   * Removes the specific style from all tab items.
+   * This method removes the 'text-[#ea2a49]' class from all elements with the class 'tab-item'.
+   */
+  _removeTabsStyle() {
+    this._parentElement.querySelectorAll(".tab-item").forEach((tab) => {
+      tab.classList.remove("text-[#ea2a49]");
+    });
+  }
 }
 const tabsView = new TabsView();
 export default tabsView;
